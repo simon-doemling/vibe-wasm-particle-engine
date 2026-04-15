3acb4f76430d748b8716baf8d166e018c2850a06
simon-doemling <technomsmon@gmail.com>
2026-04-15 16:43:23 +0200

    Setup GitHub Pages Deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/vibe-wasm-particle-engine/', 
+  
   server: {
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin',
       'Cross-Origin-Embedder-Policy': 'require-corp',
     },
   },
+  preview: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+    },
+  }
 });
